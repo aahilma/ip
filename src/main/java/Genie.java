e77d67c021907d711e16cf78490199c08de40664
Aahil <aahilma2004@gmail.com>
2026-08-24 00:34:04 +0530

    level 5 complete

--- src/main/java/Genie.java
+++ src/main/java/Genie.java
@@ -1,83 +1,188 @@
 import java.util.Scanner;
 
+
 public class Genie {
     public static void main(String[] args) {
-
-        String divider = "____________________________________________________________\n";
-        String banner = "Hello! I'm Genie\n"
-                + "What can I do for ?\n";
-        System.out.println(banner);
+        String divider = "    ____________________________________________________________";
+        System.out.println(divider);
+        System.out.println("     Hello! I'm Genie\n     What can I do for you?");
+        System.out.println(divider);
 
         Scanner scanner = new Scanner(System.in);
-        String userInput;
-        String[] h = new String[100];
-        boolean[] comp = new boolean[100];
+
+
+        Task[] tasks = new Task[100];
         int count = 0;
 
         while(true) {
-            userInput = scanner.nextLine();
+            String userInput = scanner.nextLine();
             String[] commands = userInput.split(" ");
 
             System.out.println(divider);
 
             if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("     Bye! See you again soon!");
+                System.out.println(divider);
                 break;
-            }
 
-            if (userInput.equalsIgnoreCase("list")) {
-                System.out.println("Here are your tasks:\n");
+            } else if (userInput.equalsIgnoreCase("list")) {
+                System.out.println("     Here are your tasks:");
                 for (int i = 0; i < count; i++) {
-                    String line = "     " + (i + 1) + ". ";
-                    if (comp[i]) {
-                        line += "[ X ] ";
-                    }
-                    else {
-                        line += "[ ] ";
-                    }
-                    line += h[i];
-                    System.out.println(line);
+                    System.out.println("     " + (i + 1) + "." + tasks[i].toString());
                 }
-                System.out.println("\n");
-                System.out.println(divider);
-                continue;
+
+            } else if (commands[0].equalsIgnoreCase("mark")) {
+                int i = Integer.parseInt(commands[1]) - 1;
+                tasks[i].markAsDone();
+                System.out.println("     Nice! I have marked this task as done:\n");
+                System.out.println("    " + tasks[i].toString());
+            }
+            else if (commands[0].equalsIgnoreCase("unmark")) {
+                int i = Integer.parseInt(commands[1]) - 1;
+                tasks[i].markAsUndone();
+                System.out.println("     Nice! I have marked this task as undone:\n");
+                System.out.println("    " + tasks[i].toString());
+            }
+            else if (commands[0].equalsIgnoreCase("todo")) {
+
+                if (commands.length == 1) {
+                    System.out.println("     OOPS!!! The description of a todo cannot be empty.");
+                    continue;
+                }
+
+                String description = userInput.substring(5).trim();
+                tasks[count++] = new ToDo(description);
+
+                System.out.println("     Got it. I've added this task:");
+                System.out.println("       " + tasks[count - 1].toString());
+                System.out.println("     Now you have " + count + " tasks in the list.");
+            }
+            else if (commands[0].equalsIgnoreCase("deadline")) {
+                if (commands.length == 1) {
+                    System.out.println("     OOPS!!! The description of a deadline cannot be empty.");
+                    continue;
+                }
+
+                String info = userInput.substring(9).trim();
+
+
+                String[] parts = info.split(" /by ");
+                String description = parts[0];
+                String by = parts[1];
+
+                tasks[count++] = new Deadline(description, by);
+
+                System.out.println("     Got it. I've added this task:");
+                System.out.println("       " + tasks[count - 1].toString());
+                System.out.println("     Now you have " + count + " tasks in the list.");
+
+            }
+            else if (commands[0].equalsIgnoreCase("event")) {
+                if (commands.length == 1) {
+                    System.out.println("     OOPS!!! The description of an event cannot be empty.");
+                    continue;
+                }
+
+                String info = userInput.substring(6).trim();
+
+
+                String[] parts = info.split(" /from ");
+                String description = parts[0];
+
+
+                String[] timeParts = parts[1].split(" /to ");
+                String from = timeParts[0];
+                String to = timeParts[1];
+
+                tasks[count++] = new Event(description, from, to);
+
+                System.out.println("     Got it. I've added this task:");
+                System.out.println("       " + tasks[count - 1].toString());
+                System.out.println("     Now you have " + count + " tasks in the list.");
+            }
+            else {
+                System.out.println("     OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
 
-            if (commands[0].equals("mark")) {
-                comp[Integer.parseInt(commands[1]) - 1] = true;
-                String txt = "Nice! I have marked the following as done: \n";
-                System.out.println(txt);
-                String txt2 = "[ X ] ";
-                txt2 += h[Integer.parseInt(commands[1]) - 1];
-                System.out.println(txt2);
+            if (!userInput.equalsIgnoreCase("bye")) {
                 System.out.println(divider);
-                continue;
             }
-
-            if (commands[0].equals("unmark")) {
-                comp[Integer.parseInt(commands[1]) - 1] = false;
-                String txt = "Nice! I have unmarked the following: \n";
-                System.out.println(txt);
-                String txt2 = "[  ] ";
-                txt2 += h[Integer.parseInt(commands[1]) - 1];
-                System.out.println(txt2);
-                System.out.println(divider);
-                continue;
-            }
-
-
-
-            h[count] = userInput;
-            count++;
-
-
-            System.out.println("Genie added: " + userInput);
-            System.out.println(divider);
-
-
         }
-
-        System.out.println("Bye! See you again soon!");
-
         scanner.close();
+    }
+
+}
+
+
+class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]");
+    }
+
+    public String getName() {
+        return this.description;
+    }
+
+    @Override
+    public String toString() {
+        return getStatusIcon() + " " + description;
+    }
+}
+
+class ToDo extends Task {
+    public ToDo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
+
+class Event extends Task {
+    protected String from;
+    protected String to;
+
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+    }
+}
+
+
+class Deadline extends Task {
+    protected String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
     }
 }
